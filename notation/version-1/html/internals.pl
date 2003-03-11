f813e15bbad7bd8bb1543fe69ea07f452c6152b2
mhucka <mhucka@caltech.edu>
2003-03-11 05:19:56 +0000

    This commit was generated by cvs2svn to compensate for changes in r89, which included commits to RCS files with non-trunk default branches.

---
+++ notation/version-1/html/internals.pl
@@ -0,0 +1,54 @@
+# LaTeX2HTML 99.2beta8 (1.46)
+# Associate internals original text with physical files.
+
+
+$key = q/subsub:inter/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:inclusion/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:introduction/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:list-attributes/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:simple-attributes/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsub:intra/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:representing/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:simple-types/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:links/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:complex-attributes/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:xml/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:schemas/;
+$ref_files{$key} = "$dir".q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ notation/version-1/html/labels.pl
@@ -0,0 +1,109 @@
+# LaTeX2HTML 99.2beta8 (1.46)
+# Associate labels original text with physical files.
+
+
+$key = q/subsub:inter/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:inclusion/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:introduction/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:list-attributes/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:simple-attributes/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsub:intra/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:representing/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:simple-types/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:links/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:complex-attributes/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:xml/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:schemas/;
+$external_labels{$key} = "$URL/" . q|notation.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 99.2beta8 (1.46)
+# labels from external_latex_labels array.
+
+
+$key = q/subsub:inter/;
+$external_latex_labels{$key} = q|3.3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:inclusion/;
+$external_latex_labels{$key} = q|3.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:introduction/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:list-attributes/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:simple-attributes/;
+$external_latex_labels{$key} = q|3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsub:intra/;
+$external_latex_labels{$key} = q|3.3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:representing/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:simple-types/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:links/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub:complex-attributes/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:xml/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:schemas/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+1;
+
